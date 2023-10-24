503ee050596632520a8bec46d406fb91653e6c25
kurt <kurttfrancisco@gmail.com>
2023-10-24 12:34:05 +0800

    finished colorpicker, reset, change color on mouse hover

--- odin-js/etch-a-sketch/app.js
+++ odin-js/etch-a-sketch/app.js
@@ -1,6 +1,33 @@
 const gridContainer = document.getElementById("grid-container");
+const btnReset = document.getElementById("reset");
+const colorPicker = document.getElementById("color");
 
 let gridSize = 16;
+let isMouseDown = false;
+
+const updateGridSize = (newGridSize) => {
+  gridSize.innerHTML = newGridSize;
+  sizeValue.innerHTML = `${newGridSize} x ${newGridSize}`;
+};
+
+colorPicker.addEventListener("input", (event) => {
+  const selectedColor = event.target.value;
+  document.querySelector(".grid").forEach((grid) => {
+    grid.style.backgroundColor = selectedColor;
+  });
+});
+
+// const rangeSize = () => {
+//   sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+// sizeSlider.onchange = (e) => changeSize(e.target.value)
+// }
+
+// Reset Etch-A-Sketch
+btnReset.addEventListener("click", () => {
+  gridContainer.innerHTML = "";
+
+  createGrid(gridSize);
+});
 
 const createGrid = () => {
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
@@ -10,9 +37,17 @@ const createGrid = () => {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     gridContainer.appendChild(grid);
+
+    grid.addEventListener("mousedown", () => {
+      isMouseDown = true;
+      grid.style.backgroundColor = colorPicker.value;
+    });
+
+    grid.addEventListener("mouseover", () => {
+      isMouseDown = true;
+      grid.style.backgroundColor = colorPicker.value;
+    });
   }
 };
 
 createGrid();
-
-// const colorChange = (event) => {};
